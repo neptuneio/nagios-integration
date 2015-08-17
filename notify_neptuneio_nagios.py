85d8c7e4dcc5f7a48761045981745dd596197f5f
Buchi Reddy Busi Reddy <brbusireddy@neptune.io>
2015-08-17 13:29:11 -0700

    Committing the Nagios + Neptune.io integration supporting files. Excited to support all Nagios users.. :)

---
+++ notify_neptuneio_nagios.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+"""
+Script to send Nagios events to Neptune.io
+
+This script is designed to work as a notification command for Nagios,
+which reads the event details from environment and pushes that event
+to Neptune.io whenever a notification is triggered. Please read the
+integration guide for more details on how to use this script.
+"""
+
+from optparse import OptionParser
+
+import os
+import sys
+import requests
+import simplejson as json
+
+API_BASE_URL = 'https://www.neptune.io/api/v1/trigger/channel/nagios/'
+
+
+def send_to_neptune(key, e):
+    try:
+        response = requests.post(API_BASE_URL + key, data=json.dumps(e), verify=True)
+        if response.status_code != 200:
+            print("Failed to send the event to Neptune.io; status: %d, msg: %s", response.status_code, response.text)
+    except Exception:
+        _, e, _ = sys.exc_info()
+        print("Failed to send the event to Neptune.io; Error: %s", repr(e))
+
+
+if __name__ == "__main__":
+    p = OptionParser()
+    p.add_option('--api_key', '-k', type='string')
+    p.add_option('--type', '-t', type='string')
+    (options, arguments) = p.parse_args()
+
+    api_key = options.api_key
+    if not api_key:
+        print("Neptune.io API key is missing. Please specify it with --api_key option.")
+        sys.exit(1)
+
+    env = os.environ.copy()
+    event = {k[7:]: v for k, v in env.iteritems() if k.startswith("NAGIOS_")}
+
+    if options.type:
+        event['type'] = options.type
+
+    send_to_neptune(api_key, event)
